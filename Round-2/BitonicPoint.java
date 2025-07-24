a1b965eeb1b148c1bd071fe0596cc15f1cc04044
anees215 <21ec267@students.stjosephs.ac.in>
2025-07-24 12:35:57 +0530

    Bitonic Project

--- Round-2/BitonicPoint.java
+++ Round-2/BitonicPoint.java
@@ -1,19 +1,19 @@
 public class BitonicPoint {
     public static void main(String[] args) {
-        int[] arr = {1,4,5,9,14,6,1};
+        int[] arr = {1,3,8,12,4,2};
         int result = findBitonicPoint(arr);
         System.out.println(result);
     }
 
     private static int findBitonicPoint(int[] arr) {
         int left = 0;
-        int right = arr.length-1; 
-
+        int right = arr.length-1;
+        
         while(left<=right){
             int mid = (left+right)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return arr[mid]; 
-            else if (arr[mid-1]>arr[mid]) right = mid-1;
-            else left=mid+1;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return arr[mid];
+            else if(arr[mid-1]>arr[mid]) right = mid-1;
+            else left = mid+1;
         }
 
         return -1;
