513be2c3b2dd7251b995a65500c7592850d6e539
anees215 <21ec267@students.stjosephs.ac.in>
2025-07-24 18:15:46 +0530

    Find the index

--- Round-2/EquilibriumIndex.java
+++ Round-2/EquilibriumIndex.java
@@ -1,8 +1,8 @@
 public class EquilibriumIndex {
     public static void main(String[] args) {
-        int[] nums = { 3, 4, 5, 1, 8, 12, 1 };
+        int[] nums = {3,4,9,6,1};
         int index = findEquilibriumIndex(nums);
-        System.out.println(index);
+        System.out.println(index); 
     }
 
     private static int findEquilibriumIndex(int[] nums) {
