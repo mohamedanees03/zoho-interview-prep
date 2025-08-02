0688f6f0b3f24bef3b4eb705bdfaeded3c61ecad
anees215 <21ec267@students.stjosephs.ac.in>
2025-08-02 14:00:16 +0530

    Love you

--- Round-2/CheckAnagram.java
+++ Round-2/CheckAnagram.java
@@ -2,8 +2,6 @@ class CheckAnagram {
     public static void main(String[] args) {
         String input1 = "aabbcd";
         String input2 = "abcdba";
-        String anees = "Shifa";
-        
         boolean isAnagram = findAnagram(input1, input2);
         System.out.println(isAnagram);  // Expected output: true
     }
